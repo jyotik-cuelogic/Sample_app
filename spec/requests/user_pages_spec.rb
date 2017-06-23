9e0a5a68a5470fa5848109b6a2944048becd620d
Jyoti <Jyoti.kumbhar@cuelogic.co.in>
2017-06-23 12:30:05 +0530

    7th chapter exercise

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,6 +17,7 @@ end
 
 
 describe "signup" do
+ 
  before {visit signup_path}
  let(:submit){"Create my account"}
 
@@ -37,6 +38,29 @@ describe "signup" do
 	it "should create a user" do
 	expect { click_button submit }.to change(User, :count).by(1)
 	end
+    
+    describe "after submission" do
+      before {click_button submit}
+
+      it {should have_selector('title',text: 'Sign up')}
+      it {should have_selector('error')}
+    end
+    
+    describe "after submission" do
+      before {click_button submit}
+
+      it {should have_selector('title',text: 'Sign up')}
+      it {should have_selector('error')}
+    end
+
+    describe "after saving the user" do
+ 	  before {click_button submit}
+      let(:user){User.find_by_email('user@example.com')}
+
+      it {should have_selector('title',text: 'Sign up')}
+      it {should have_selector('div.alert.alert-success', text: 'Welcome')}
+    end
+
    end
  end
 end
